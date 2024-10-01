4bb83c37a0fe231153bce53e0e695ff35f470183
Clint Bush <clint@madebyclint.com>
2024-10-01 16:57:43 -0700

    Initial Commit

---
+++ SimpleGroceryListTests/SimpleGroceryListTests.swift
@@ -0,0 +1,16 @@
+//
+//  SimpleGroceryListTests.swift
+//  SimpleGroceryListTests
+//
+//  Created by Clint Bush on 10/1/24.
+//
+
+import Testing
+
+struct SimpleGroceryListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
